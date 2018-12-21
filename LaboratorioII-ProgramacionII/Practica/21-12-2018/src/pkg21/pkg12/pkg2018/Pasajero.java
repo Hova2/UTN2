b4f386f86061391193ca9f41ae02130d2cab4753
Juani <nacho.mone@gmail.com>
2018-12-21 16:38:52 -0300

    21-12-2018

--- LaboratorioII-ProgramacionII/Practica/21-12-2018/src/pkg21/pkg12/pkg2018/Pasajero.java
+++ LaboratorioII-ProgramacionII/Practica/21-12-2018/src/pkg21/pkg12/pkg2018/Pasajero.java
@@ -5,30 +5,59 @@
  */
 package pkg21.pkg12.pkg2018;
 
+import java.util.Objects;
+
 /**
  *
  * @author iguglielmone_mecon
  */
 public class Pasajero {
     
+    /*Atributos privados y de instancia: _apellido (String), 
+    _nombre (String), _dni (String), _esPlus (Boolean)*/
+    
     private String _apellido;
     private String _nombre;
     private String _dni;
     private Boolean _esPlus;
     
-    public Pasajero(){
+    /*Realizar un constructor parametrizado que permita inicializar todos los 
+    atributos de la clase llamando a otras dos sobrecargas. No se deberá repetir código.*/
+    
+    public Pasajero(Boolean esPl){
+        this._esPlus = esPl;
     }
     
-    public Pasajero(String ape, String nom){
-        this();
+    public Pasajero(String dni, Boolean esPl){
+        this(esPl);
+        this._dni = dni;
+    }
+    
+    public Pasajero(String ape, String nom, String dni, Boolean esPl){
+        this(dni,esPl);
         this._apellido = ape;
         this._nombre = nom;
     }
     
-    public Pasajero(String ape, String nom, String dni, Boolean esPl){
-        this(ape,nom);
-        this._dni = dni;
-        this._esPlus = esPl;
+    /*Crear el método CompararPasajeros que recibirá como parámetro 
+    una instancia de la clase. Retornará verdadero cuando el dni de ambas instancias sean iguales.*/
+
+    public Boolean compararPasajeros(Pasajero pasa){
+        return this.equals(pasa);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        return ((Pasajero)obj)._dni.equals(this._dni);
+    }
+
+    /*Un getter llamado getInfo permitirá construir un String con toda la información 
+    del pasajero y la retornará. Utilizar StringBuilder.*/
+    
+    public String getInfo(){
+        StringBuilder sb=new StringBuilder();
+        sb.append(super.mostrar()).append("\n");
+    
     }
     
 }
