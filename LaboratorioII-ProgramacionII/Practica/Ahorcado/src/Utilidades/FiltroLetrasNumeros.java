c0232e91b3c18b9997b803c7528deef59e4e83a4
Hova2 <nacho.mone@gmail.com>
2019-04-03 20:25:34 -0300

    03-04-2019-01

---
+++ LaboratorioII-ProgramacionII/Practica/Ahorcado/src/Utilidades/FiltroLetrasNumeros.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilidades;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+
+/**
+ *
+ * @author iguglielmone_mecon
+ */
+public class FiltroLetrasNumeros extends DocumentFilter{
+
+    @Override
+    public void replace(FilterBypass fb, int offset, int length, String string, AttributeSet attrs) throws BadLocationException {
+        String texto = fb.getDocument().getText(0,fb.getDocument().getLength());
+        texto += string;
+        if(texto.matches("^[a-zA-Z0-9]*$")){
+            super.insertString(fb, offset, string, attrs); //To change body of generated methods, choose Tools | Templates.
+        }else{
+        }
+    }
+
+    @Override
+    public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+        String texto = fb.getDocument().getText(0,fb.getDocument().getLength());
+        texto += string;
+        if(texto.matches("^[a-zA-Z0-9]*$")){
+            super.insertString(fb, offset, string, attr); //To change body of generated methods, choose Tools | Templates.
+        }else{
+        }
+        
+        
+    }
+
+    @Override
+    public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+        super.remove(fb, offset, length); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
